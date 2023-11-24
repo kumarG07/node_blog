f91b6d3650fa5bf77bd63abba502f4e94053acf2
Kumar G <141736180+kumarG07@users.noreply.github.com>
2023-11-24 20:24:47 +0530

    Add files via upload

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,1 +1,17 @@
+import { createConnection, Db } from 'typeorm';
+
+export const connectDB = async () => {
+  return  createConnection({
+        type: 'mysql',
+        host: 'localhost',
+        port: 3306,
+        username: 'root',
+        password: '',
+        database: 'blog_db',
+        synchronize: true,
+        logging: true,
+        entities: [__dirname + '/../tables/*.ts'],
+    })
+
+}
 
