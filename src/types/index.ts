abc740308d54a827eb791826674c6d181d5a3dfb
Kumar G <141736180+kumarG07@users.noreply.github.com>
2023-11-24 20:28:36 +0530

    Add files via upload

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,1 +1,18 @@
+export type Id = {
+    id: number
+    creationDate: Date
+}
 
+export type Article = {
+    content: string
+    title: string
+    nickname: string
+}
+
+
+export type Comment = {
+    content: string
+    nickname: string
+    articleId: number,
+    parentId?:number
+}
